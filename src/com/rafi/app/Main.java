fd3fd8fdf3415dfcfe455f981aff77e8f4acc112
Abdur <abdur.rafi@monstar-lab.com>
2015-08-28 14:29:01 +0900

    Initial Commit (Base)

---
+++ src/com/rafi/app/Main.java
@@ -0,0 +1,15 @@
+package com.rafi.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Student student = new Student(123, "Moga");
+		
+		System.out.println(student);
+		
+		student.hanashi();
+
+	}
+
+}
